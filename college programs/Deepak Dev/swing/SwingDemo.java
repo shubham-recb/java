8f8ea28d86258c825d1ed3c8aebbcb913e0034aa
shubham-recb <55327190+shubham-recb@users.noreply.github.com>
2019-12-24 06:56:05 -0800

    Add files via upload

---
+++ college programs/Deepak Dev/swing/SwingDemo.java
@@ -0,0 +1,13 @@
+public class SwingDemo
+{
+	public static void main(String args[])
+	{
+		JFrame frame = new JFrame();
+		JButton button = new JButton();
+		
+		button.setBounds(100,100,100,60);
+		frame.add(button);
+		frame.setSize(300,300);
+		f.setVisible(true);
+	}
+}
